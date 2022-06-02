a67356d791b9cd897b92a5341188019d006f307f
Artur <petin.art@gmail.com>
2022-06-02 14:47:37 +0300

    Initial Commit

---
+++ SpringAnimateHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimateHW
+//
+//  Created by Артур Петин on 02.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
